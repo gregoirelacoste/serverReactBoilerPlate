305cc6280a2f5d0d8c86b1a1e02097fab1927c2e
Lacoste <gregoire@level-one.fr>
2020-09-29 15:39:59 +0200

    front améliore

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.jsx",
   mode: "development",
   module: {
     rules: [
@@ -20,14 +20,15 @@ module.exports = {
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
+    filename: "declarer-sinistre.main.[hash].js",
+    chunkFilename: 'declarer-sinistre.chunk.[name].[hash].js',
+    path: path.join(__dirname, 'build', 'static'),
+    publicPath: '/'
   },
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
+    publicPath: "http://localhost:3000/build/",
     hotOnly: true
   },
   plugins: [new webpack.HotModuleReplacementPlugin()]
