a2551d9811d6b60f273ac9e75f46264abc3b01c7
Gregoire <gregoire@level-one.fr>
2020-09-30 12:07:40 +0200

    typescript server prod et recette

--- config/env.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-if (!process.env.NODE_ENV) {
-  throw new Error(
-    `Vous n'avez pas défini la variable d'environnement NODE_ENV`
-  )
-}
--- webpack/config.optimization.js
+++
@@ -1,9 +0,0 @@
-const optimization = {
-  minimize: process.env.NODE_ENV === "production",
-  splitChunks: {
-    chunks: "all",
-    maxInitialRequests: 10,
-    minSize: 20,
-  },
-};
-module.exports = optimization;
