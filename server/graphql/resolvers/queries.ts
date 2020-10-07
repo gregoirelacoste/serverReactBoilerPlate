ae01c28ddb5e532e16fd12a7e1ec4f8bc7a200ca
Gregoire <gregoire@level-one.fr>
2020-10-07 12:18:08 +0200

    install codegen graphql

--- server/graphql/resolvers/queries.ts
+++ server/graphql/resolvers/queries.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client/scripts/default-index";
 
 export const queries = (repo: PrismaClient) => ({
-  users: async (_parent: any, args) => await repo.user.findMany(),
+  users: async (_parent: any) => await repo.user.findMany(),
   dossiers: async () => await repo.dossier.findMany(),
 });
