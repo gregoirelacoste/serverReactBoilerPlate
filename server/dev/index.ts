ec9c880b560e4cb74d877fb51431361b94116d1e
Gregoire <gregoire@level-one.fr>
2021-01-06 09:46:46 +0100

    fix issue on dev server

--- server/dev/index.ts
+++ server/dev/index.ts
@@ -1,12 +1,14 @@
 import { Express } from "express";
 import { NODE_ENV } from "../../config/env";
 import { config } from "../config";
-const webpack = require("webpack");
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const getWebpackConfig = require("../../webpack/webpack.config");
+
 
 export const devPath = (app: Express, express: any) => {
   if (NODE_ENV !== "development") return;
+
+  const webpack = require("webpack");
+  const webpackDevMiddleware = require("webpack-dev-middleware");
+  const getWebpackConfig = require("../../webpack/webpack.config");
 
   const webpackConfig = getWebpackConfig({}, { mode: "development" });
   const compiler = webpack(webpackConfig);
