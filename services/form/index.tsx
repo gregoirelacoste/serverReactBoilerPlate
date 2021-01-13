da899ce0318bd92e6f3bbec56c129a7c7bf5fe12
Gregoire <gregoire@level-one.fr>
2021-01-13 11:28:51 +0100

    simple form services

---
+++ services/form/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Box, Grid, Typography } from "@material-ui/core";
+import { ContentFieldTypes } from "./types/form.types";
+import ChooseFields from "./FormMain/ChooseFields";
+import { HandleChangeType } from "./FormMain/handleChange";
+import { getOptionnalFields } from "./FormMain/getOptionnalFields";
+
+interface ForMainProps {
+  state: any;
+  fields: ContentFieldTypes<any>[];
+  handleChange: HandleChangeType<any>;
+  onValidation?: (any: any) => any;
+  loading?: boolean;
+  title: string;
+}
+
+const TheForm = ({ fields, state, handleChange, title }: ForMainProps) => {
+  if (!fields) return null;
+  console.log(state);
+  return (
+    <>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Typography variant={"h3"} component={"h3"} color={"primary"}>
+            {title}
+          </Typography>
+          <Box m={3} />
+        </Grid>
+        {fields?.map((field, i) => {
+          const fieldName = field.name;
+          const fieldValue = state?.values?.[fieldName];
+          const fieldError = state?.errors?.[fieldName]?.[0];
+          getOptionnalFields({ field, data: state });
+
+          return (
+            <ChooseFields
+              key={i}
+              field={field}
+              size={field.size}
+              value={fieldValue}
+              error={fieldError}
+              handleChange={handleChange}
+            />
+          );
+        })}
+      </Grid>
+      <Box m={3} />
+    </>
+  );
+};
+export default TheForm;
